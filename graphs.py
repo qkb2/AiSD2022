3c724dd44aa2d7794b050e910a83c1a1fd108945
qkb2 <53836634+qkb2@users.noreply.github.com>
2022-05-19 13:22:42 +0200

    Small commit with totally not tested hcycle

---
+++ graphs.py
@@ -0,0 +1,191 @@
+import random
+
+
+def edge_list_to_vertex_list(edge_list: list):
+    max_key = 0
+    for el in edge_list:
+        if el[0] > max_key:
+            max_key = el[0]
+        if el[1] > max_key:
+            max_key = el[1]
+    return [i for i in range(max_key+1)]
+    # assuming that since it's impossible to create "solitary" nodes via edge list
+    # it's better to just create all the possible nodes that could exist
+
+
+
+class UndirAdjMatrix():
+    def __init__(self) -> None:
+        self.V = 0
+        self.E = 0
+        self.matrix = []
+        self.is_visited = [] # vertices start at 0
+
+    def __repr__(self) -> str:
+        repr = ''
+        for i in range(self.V):
+            for j in range(self.V):
+                repr += str(self.matrix[i][j])+'\t'
+            repr += '\n'
+        return repr
+    
+    def create_from_edge_list(self, edge_list: list, vertex_list: list) -> None:
+        self.V = len(vertex_list) # from 0 to n not included
+        self.E = len(edge_list)
+
+        self.matrix = [[0 for _ in range(self.V)] for _ in range(self.V)]
+        for el in edge_list:
+            self.matrix[el[0]][el[1]] = 1
+            self.matrix[el[1]][el[0]] = 1
+
+        self.is_visited = [False for _ in vertex_list]
+
+    def create_matrix_wrapper(self, edge_list: list):
+        vertex_list = edge_list_to_vertex_list(edge_list)
+        self.create_from_edge_list(edge_list, vertex_list)
+
+    def create_random_undir_graph(self, n: int, D: float) -> None:
+        self.V = n
+        self.E = int(n*(n-1)*D//2)
+        self.matrix = [[0 for _ in range(self.V)] for _ in range(self.V)]
+        self.is_visited = [False for _ in range(self.V)]
+        c = 0
+        c_panic = 0
+        random.seed()
+        while c < self.E and c_panic < 10*self.E:
+            c_panic += 1
+            x = random.randint(0, self.V-1)
+            y = random.randint(0, self.V-1)
+            if self.matrix[x][y] == 0 and x != y:
+                self.matrix[x][y] = 1
+                self.matrix[y][x] = 1
+                c += 1
+
+    def __hamiltonian(self, v: int) -> bool:
+        self.is_visited[v] = True
+        self.visited = 1
+        helper_list = self.matrix[v]
+        for i in range(self.V):
+            if  helper_list[i] == 1 and i == self.start and self.visited == self.V:
+                return True
+            if not self.is_visited[i]:
+                db = self.__hamiltonian(i)
+                if db:
+                    self.hpath[self.k] = v
+                    self.k += 1
+                    return True
+        self.is_visited[v] = False
+        self.visited -= 1
+        return False
+
+    def hamiltonian_cycle(self) -> bool:
+        self.hpath = [-1 for _ in range(self.V)]
+        self.is_visited = [False for _ in range(self.V)]
+        self.hpath[0] = 0
+        self.start = 0
+        self.visited = 0
+        self.k = 1
+        is_ham = self.__hamiltonian(self.start)
+        return is_ham
+
+
+class AdjList:
+    def __init__(self) -> None:
+        self.V = 0
+        self.E = 0
+        self.out_nodes = {}
+        self.is_visited = []
+
+    def __repr__(self) -> str:
+        repr = ''
+        for i in range(self.V):
+            repr += str(i)+' -> '+str(self.out_nodes[i])+'\n'
+        repr += '\n'
+        return repr
+
+    def create_from_edge_list(self, edge_list: list, vertex_list: list) -> None:
+        self.V = len(vertex_list)
+        self.E = len(edge_list)
+
+        for v in vertex_list:
+            self.out_nodes.update({v: []})
+            self.is_visited.append(False)
+
+        for el in edge_list:
+            u = el[0]
+            v = el[1]
+            self.out_nodes[u].append(v)
+
+        for i in range(self.V):
+            self.out_nodes[i].sort()
+
+    def create_list_wrapper(self, edge_list: list):
+        vertex_list = edge_list_to_vertex_list(edge_list)
+        self.create_from_edge_list(edge_list, vertex_list)
+
+    def create_random_dir_graph(self, n: int, D: float) -> None:
+        self.V = n
+        self.E = int(n*(n-1)*D)
+
+        for v in range(self.V):
+            self.out_nodes.update({v: []})
+            self.is_visited.append(False)
+
+        c = 0
+        c_panic = 0
+        random.seed()
+        while c < self.E and c_panic < 10*self.E:
+            c_panic += 1
+            x = random.randint(0, self.V-1)
+            y = random.randint(0, self.V-1)
+            if y not in self.out_nodes[x] and y != x:
+                self.out_nodes[x].append(y)
+                c += 1
+        
+        for i in range(self.V):
+            self.out_nodes[i].sort()
+
+    def __hamiltonian(self, v: int) -> bool:
+        self.is_visited[v] = True
+        self.visited = 1
+        for i in self.out_nodes[v]:
+            if i == self.start and self.visited == self.V:
+                return True
+            if not self.is_visited[i]:
+                db = self.__hamiltonian(i)
+                if db:
+                    self.hpath[self.k] = v
+                    self.k += 1
+                    return True
+        self.is_visited[v] = False
+        self.visited -= 1
+        return False
+
+    def hamiltonian_cycle(self) -> bool:
+        self.hpath = [-1 for _ in range(self.V)]
+        self.is_visited = [False for _ in range(self.V)]
+        self.hpath[0] = 0
+        self.start = 0
+        self.visited = 0
+        self.k = 1
+        is_ham = self.__hamiltonian(self.start)
+        return is_ham
+
+    
+
+
+if __name__ == "__main__":
+
+    test_list = [[1, 2], [1, 3], [3, 4], [3, 5], [4, 1]]
+    adj_mat = UndirAdjMatrix()
+    adj_mat.create_matrix_wrapper(test_list)
+    print(adj_mat)
+    adj_list = AdjList()
+    adj_list.create_list_wrapper(test_list)
+    print(adj_list)
+    rand_mat = UndirAdjMatrix()
+    rand_mat.create_random_undir_graph(10, 0.5)
+    print(rand_mat)
+    rand_list = AdjList()
+    rand_list.create_random_dir_graph(10, 0.5)
+    print(rand_list)
---
+++ main.py
@@ -0,0 +1,169 @@
+import graphs
+# import tests
+from os.path import isfile
+
+class UserPrompt:
+    def __init__(self) -> None:
+        self.edge_list = []
+        self.V = 0
+        self.E = 0
+
+
+    def edge_list_input(self):
+        self.edge_list = []
+        # must be changed to accept different type of input (lines of pairs of ints)
+        print(
+            "Warning! This program accepts only integer values. Negative integers will be converted to their absolute "
+            "value. The array cannot be empty.\nChoose natural key values. If you want to stop adding the edges press "
+            "enter without typing any values. Note: the first key will always be assumed to be 1 and the last will be\n"
+            "assumed to be the greatest number typed. Keys not used will still have their nodes created if they fall\n"
+            "in range of 1 and max key, but they will not affect the usability of an algorithm.")
+        while True:
+            x = input(
+                "Please enter your numbers as pairs, entering one pair at the time, one whitespace between each number: "
+                "and one new line between each pair\n")
+            if x == '':
+                if len(self.edge_list) != 0:
+                    return True
+                return False
+
+            array = x.split()
+            try:
+                array = list(map(int, array))
+            except ValueError:
+                print("The input seems to include non-numbers")
+                continue
+            
+            if array[0] == 0 or array[1] == 0:
+                print("The key cannot be 0")
+                continue
+
+            if len(array) == 2:
+                self.edge_list.append(array)
+            else:
+                print("Too many numbers in a row.")
+
+    def edge_list_from_file(self):
+        self.edge_list = []
+        # must be changed to accept different type of input (lines of pairs of ints)
+        print(
+            "Warning! This program accepts only integer values. Negative integers will be converted to their absolute "
+            "values. If the file contains any discrepencies, zeroes etc. there will be an error raised.")
+        faddr = ''
+        while True:
+            faddr = input("Please enter a correct path to a file: ")
+            if isfile(faddr) and faddr.endswith(".txt"):
+                break
+            else:
+                print("Path incorrect.")
+            
+        with open(faddr, "r") as fread:
+            x = fread.readline()
+            if x == '':
+                return False
+            array = x.split()
+            try:
+                array = list(map(int, array))
+            except ValueError:
+                print("Values corrupted. Choose a different file.")
+                return False
+                
+            if array[0] == 0:
+                print("The graph cannot be empty. Choose a different file.")
+                return False
+
+            self.V = array[0]
+            self.E = array[1]
+            c = 0
+         
+            while c < self.E:
+                # print(c)
+                c += 1
+                x = fread.readline()
+                if x == '':
+                    if len(self.edge_list) != 0:
+                        return True
+                    return False
+
+                array = x.split()
+                try:
+                    array = list(map(int, array))
+                except ValueError:
+                    continue
+                
+                if array[0] == 0 or array[1] == 0:
+                    continue
+
+                if len(array) == 2:
+                    self.edge_list.append(array)
+
+    def user_loop(self, s: str):
+        if s == 'file loop':
+            options_looper = self.edge_list_from_file()
+            while not options_looper:
+                options_looper = self.edge_list_from_file()
+        else:
+            options_looper = self.edge_list_input()
+            while not options_looper:
+                options_looper = self.edge_list_input()           
+
+
+    def display_results(self):
+        mat = graphs.AdjMatrix()
+        mat.create_matrix_wrapper(self.edge_list)
+        print("Adjacency matrix:")
+        print(mat)
+        print("Matrix of a graph following the prof. Szachniuk's convention:")
+        print(mat.get_str())
+        print("Sorting the graph with both algorithms.")
+        arr1 = mat.kahn_top_sort()
+        arr2 = mat.dfs_top_sort()
+        if len(arr1) != len(arr2):
+            print("An error occured. Exiting...")
+            return
+        if len(arr1) == 0:
+            print("Graph cannot be sorted.")
+            return
+        print("Topologically sorted arrays gotten by:\nKahn's sort: {}\nDFS-based sort: {}".format(arr1, arr2))
+
+
+    def main_loop(self) -> None:
+        while True:
+            s = input(
+                "Choose one of the following options: [testing, user input, file input, exit]. ").lower()
+            if s == "user input":
+                self.user_loop(s)
+                self.display_results()
+                s = input(
+                    "If you want to exit the program, enter [exit]. Otherwise press Enter to try again. ")
+                if s == "exit":
+                    return
+
+            elif s == "testing":
+                print(
+                    "Warning! Auto-testing takes a long time to complete and its completion is dependent on the "
+                    "user's computer specs. The data will be generated in the data.csv file.")
+                print(
+                    "Now wait for the data collection to complete. After that, the program will automatically shut "
+                    "down.")
+                # tests.testing_suit()
+                return
+            
+            elif s == "file input":
+                self.edge_list_from_file()
+                self.display_results()
+                s = input(
+                    "If you want to exit the program, enter [exit]. Otherwise press Enter to try again. ")
+                if s == "exit":
+                    return
+
+            elif s == "exit":
+                return
+
+            else:
+                print("Please enter the correct option.")
+
+
+if __name__ == '__main__':
+    prompt = UserPrompt()
+    prompt.main_loop()
